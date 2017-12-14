966af5df722ad150c97ce772aee1a0ce3e4f5343
mariaelfving <maria.elfving@gmail.com>
2017-12-14 23:07:47 +0100

    updates

--- compare.sh
+++ compare.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-./tree_distances original_msa/symmetric_0.5/symmetric_0.5.tree original_msa/symmetric_0.5
-./tree_distances original_msa/symmetric_0.5/symmetric_0.5.tree filtered_msa/symmetric_0.5
+# Bash file to compare all the generated trees to the appropriate reference tree
 
-./tree_distances original_msa/symmetric_1.0/symmetric_1.0.tree original_msa/symmetric_1.0
-./tree_distances original_msa/symmetric_1.0/symmetric_1.0.tree filtered_msa/symmetric_1.0
+./tree_distances reference_trees/symmetric_0.5.tree original_msa/symmetric_0.5
+./tree_distances reference_trees/symmetric_0.5.tree filtered_msa/symmetric_0.5
 
-./tree_distances original_msa/symmetric_2.0/symmetric_2.0.tree original_msa/symmetric_2.0
-./tree_distances original_msa/symmetric_2.0/symmetric_2.0.tree filtered_msa/symmetric_2.0
+./tree_distances reference_trees/symmetric_1.0.tree original_msa/symmetric_1.0
+./tree_distances reference_trees/symmetric_1.0.tree filtered_msa/symmetric_1.0
 
-./tree_distances original_msa/asymmetric_0.5/asymmetric_0.5.tree original_msa/asymmetric_0.5
-./tree_distances original_msa/asymmetric_0.5/asymmetric_0.5.tree filtered_msa/asymmetric_0.5
+./tree_distances reference_trees/symmetric_2.0.tree original_msa/symmetric_2.0
+./tree_distances reference_trees/symmetric_2.0.tree filtered_msa/symmetric_2.0
 
-./tree_distances original_msa/asymmetric_1.0/asymmetric_1.0.tree original_msa/asymmetric_1.0
-./tree_distances original_msa/asymmetric_1.0/asymmetric_1.0.tree filtered_msa/asymmetric_1.0
+./tree_distances reference_trees/asymmetric_0.5.tree original_msa/asymmetric_0.5
+./tree_distances reference_trees/asymmetric_0.5.tree filtered_msa/asymmetric_0.5
 
-./tree_distances original_msa/asymmetric_2.0/asymmetric_2.0.tree original_msa/asymmetric_2.0
-./tree_distances original_msa/asymmetric_2.0/asymmetric_2.0.tree filtered_msa/asymmetric_2.0
+./tree_distances reference_trees/asymmetric_1.0.tree original_msa/asymmetric_1.0
+./tree_distances reference_trees/asymmetric_1.0.tree filtered_msa/asymmetric_1.0
+
+./tree_distances reference_trees/asymmetric_2.0.tree original_msa/asymmetric_2.0
+./tree_distances reference_trees/asymmetric_2.0.tree filtered_msa/asymmetric_2.0
